2333ac24e2c4655969665c505309a52087ef6423
Harumi Terayama <hterayama@gmail.com>
2021-02-07 12:22:16 +0100

    created a post model

---
+++ models/post.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const schema = new mongoose.Schema({
+  creator: {
+    type: mongoose.Types.ObjectId,
+    required: true,
+  },
+  storeName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  itemsNeeded: [{
+    type: String,
+    required: true
+  }],
+  helper: {
+      type: mongoose.Types.ObjectId
+  },
+  status: {
+      type: String,
+      enum: [pending, offered, accepted, done]
+  }
+},
+{
+  timestamps: {
+    createdAt: 'creationDate',
+    updatedAt: 'UpdateDate'
+  }
+});
+
+module.exports = mongoose.model('User', schema);
