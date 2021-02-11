bfed114786ade6e2cccc04054bf6a28aecf712a0
Adriana Alcala <a.alcalacanales@gmail.com>
2021-02-11 21:06:05 +0100

    Edit List works

--- routes/message.js
+++ routes/message.js
@@ -2,43 +2,39 @@
 
 const express = require('express');
 const Message = require('./../models/message');
-const routeGuard = require('./../middleware/route-guard'); 
-
+const routeGuard = require('./../middleware/route-guard');
 
 const router = new express.Router();
 
-router.get('/:id',routeGuard, (req, res, next) => {
-    const id = req.params.id;
-    //find all the messages that have
-    //either id_sender==id && id_receiver==req.user
-    //other id_sender==req.user && id_receiver==id
-    //.then(messagesObject =>){
-        //res.render('message/chat-room', messagesObject)
-    //}
-    //.catch(error => {
-        //next(error);
-    //});
+router.get('/:id', routeGuard, (req, res, next) => {
+  const id = req.params.id;
+  res.render('message/chat-room');
+  //find all the messages that have
+  //either id_sender==id && id_receiver==req.user
+  //other id_sender==req.user && id_receiver==id
+  //sort by time
+  //.then(messagesObject =>){
+  //res.render('message/chat-room', messagesObject)
+  //}
+  //.catch(error => {
+  //next(error);
+  //});
 });
 
 router.post('/:id', routeGuard, (req, res, next) => {
-    const id = req.params.id;
-    const data = req.body;
-    Message.create({
-        senderId: req.user,
-        receiverId: id,
-        messageBody: data.message 
-    })      
-    .then(resource => {
-        res.render('message/chat-room');
+  const id = req.params.id;
+  const data = req.body;
+  Message.create({
+    senderId: req.user._id,
+    receiverId: id,
+    messageBody: data.message
+  })
+    .then((resource) => {
+      res.render('message/chat-room');
     })
-    .catch(error => {
-        next(error);
+    .catch((error) => {
+      next(error);
     });
 });
-
-
-
-
-
 
 module.exports = router;
